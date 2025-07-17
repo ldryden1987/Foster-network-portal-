55f3451ad7cc244e44fab9df03b233032a2d55aa
Lamont Dryden <ldryden1987@gmail.com>
2025-07-17 19:24:21 -0400

    fresh merge

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
     <div>
       <Header/>
       <div>
-        <nav>
+        <nav class= "space-x-20">
             <Link>Meet Your Future Friends!</Link>
             <Link>Applications</Link>
             <Link>About Us!</Link>
@@ -18,15 +18,15 @@ function Home() {
         </nav>
       </div>
       <div>
-        <img src="https://via.placeholder.com/600x300" alt="Main Image" />
+        <img src="https://images.squarespace-cdn.com/content/v1/66ec3b49803ab81bf84f89e4/1726789423907-OMC6SOD7GIZ2NLNGMGM9/house-cat-lying-on-floor-8529064-scaled.jpg?format=2500w" alt="Main Image" />
       </div>
 
       {/* featured pet space */}
       <h2>Feature Pets</h2>
-      <div>
-        <img src="https://via.placeholder.com/150" alt="Pet 1" />
-        <img src="https://via.placeholder.com/150" alt="Pet 2" />
-        <img src="https://via.placeholder.com/150" alt="Pet 3" />
+      <div class= "flex justify-center space-x-12">
+        <img src="https://www.battersea.org.uk/sites/default/files/animal_images/068Nz00000TRyslIAD.webp" alt="Pet 1" class= "w-64"/>
+        <img src="https://www.battersea.org.uk/sites/default/files/animal_images/068Nz00000TS2W7IAL.webp" alt="Pet 2" class="w-64" />
+        <img src="https://www.battersea.org.uk/sites/default/files/animal_images/068Nz00000U9GfNIAV-thumb.webp?id=63463.0717202510449030052" alt="Pet 3" class="w-64" />
       </div>
 
       {/* Button */}

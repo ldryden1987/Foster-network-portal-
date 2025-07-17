9ad9b5cd3d42fab794f3f1cf3c829d9ac3a8b85d
Sarah Amies <sarah.amies@outlook.com>
2025-07-17 13:53:37 -0500

    post

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 // server/server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import authRouter from './routers/authRouter.js';
+import resourceRouter from './routers/resourceRouter.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(authRouter);
 
 // Routes
 app.get('/', (req, res) => {
